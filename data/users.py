99bf46a8f40d387b4dc50f798bd8637ce81cc48e
Aleksan_09 <aleks.kuznetsov09@mail.ru>
2024-03-21 09:43:59 +0300

    U

---
+++ data/users.py
@@ -0,0 +1,30 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    nickname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    steam_level = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+
+    def __repr__(self):
+        return f'<User> id: {self.id}, nickname: {self.nickname}, steam level: {self.steam_level}'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
