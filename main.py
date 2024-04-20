01ffb0dbc9228479226304d6697cc8a64360b396
wgxnl1 <dydoca228@inbox.ru>
2024-04-20 21:06:13 +0300

    дедлайнов не сущетвует цифры придумали для порабощения людей

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, redirect, url_for, flash, send_from_directory, request
+from flask import Flask, render_template, redirect, url_for, flash, send_from_directory, make_response, jsonify
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
+from wtforms.fields.simple import EmailField
 from wtforms.validators import DataRequired, Email, EqualTo
-from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
 from data import db_session
+from data.games import Games
 from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-app.config['UPLOAD_FOLDER'] = 'static/avatars'
+app.config['UPLOAD_FOLDER'] = 'static/images/avatars'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -27,11 +28,25 @@ class LoginForm(FlaskForm):
 
 
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password_repeat = PasswordField('Repeat Password',
-                                    validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
-    submit = SubmitField('Register')
+    email = EmailField('Почта:', validators=[DataRequired()])
+    password = PasswordField('Пароль:', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль:', validators=[DataRequired()])
+    name = StringField('Имя пользователя:', validators=[DataRequired()])
+    submit = SubmitField('Войти:')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class AddFundsForm(FlaskForm):
+    card_num = StringField('Номер карты', validators=[DataRequired()])
+    validity = StringField('Срок действия', validators=[DataRequired()])
+    cvc = PasswordField('CVV/CVC', validators=[DataRequired()])
+    submit = SubmitField('Подтвердить')
 
 
 class AvatarForm(FlaskForm):
@@ -44,8 +59,10 @@ def load_user(user_id):
     return db_sess.query(User).get(user_id)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
+    db_sess = db_session.create_session()
+    games = db_sess.query(Games)
     return render_template('index.html')
 
 
@@ -66,11 +83,22 @@ def login():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
         db_sess = db_session.create_session()
         if db_sess.query(User).filter(User.email == form.email.data).first():
-            flash('Email address already registered', 'error')
-            return redirect(url_for('register'))
-        user = User(email=form.email.data)
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            nickname=form.name.data,
+            wallet=0,
+            currency=' руб.',
+            steam_level=0
+        )
         user.set_password(form.password.data)
         db_sess.add(user)
         db_sess.commit()
@@ -110,9 +138,75 @@ def account():
     return render_template('account.html', title='Account', form=form, user=current_user)
 
 
+@app.route('/community')
+def community():
+    return render_template('community.html', path='/community')
+
+
+@app.route('/info')
+def info():
+    return render_template('information.html', path='/info')
+
+
+@app.route('/help')
+def help():
+    return render_template('help.html', path='/help')
+
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html', path='/chat')
+
+
+@app.route('/wallet')
+def wallet():
+    return render_template('wallet.html', path='/wallet')
+
+
+@app.route('/addfunds/<int:money>', methods=['GET', 'POST'])
+def addfunds(money):
+    form = AddFundsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.id == current_user.id).first()
+        user.wallet += money
+        db_sess.commit()
+        return redirect('/')
+    return render_template('addfunds.html', path='/addfunds', form=form)
+
+
+@app.route('/games/<int:id>', methods=['GET', 'POST'])
+def games(id):
+    db_sess = db_session.create_session()
+    game = db_sess.query(Games).filter(Games.id == id).first()
+    return render_template('games.html', path='/games', game=game)
+
+
+@app.route('/buy/<int:id>', methods=['GET', 'POST'])
+def buy(id):
+    db_sess = db_session.create_session()
+    game = db_sess.query(Games).filter(Games.id == id).first()
+    user = db_sess.query(User).filter(User.id == current_user.id).first()
+    if user.wallet < game.sale_price:
+        return render_template('error.html', path='/games')
+    user.wallet -= game.sale_price
+    db_sess.commit()
+    return render_template('error.html', path='/buy')
+
+
 @app.route('/static/images/avatar-184.png')
 def default_avatar():
     return send_from_directory('static/images', 'avatar-184.png')
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 if __name__ == '__main__':
